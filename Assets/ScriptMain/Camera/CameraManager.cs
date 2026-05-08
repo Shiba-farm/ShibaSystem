1a1b2683876e3410c97a64d0a4ef18deae826887
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2026-05-08 13:02:19 +0700

    Update CameraManager.cs
    - remove old mechanic (press 1,2,3,4,5 to change view)

--- Assets/ScriptMain/Camera/CameraManager.cs
+++ Assets/ScriptMain/Camera/CameraManager.cs
@@ -10,32 +10,32 @@ public class CameraManager : NetworkBehaviour
     {
         if (IsOwner)
         {
-            InputHandler.Singleton.OnNumkeyTriggered += HandleCameraSwitch;
-            HandleCameraSwitch(1);
+            // InputHandler.Singleton.OnNumkeyTriggered += HandleCameraSwitch;
+            // HandleCameraSwitch(1);
         }
         else
         {
             foreach (var cam in playerCameras) cam.enabled = false;
         }
     }
-    private void HandleCameraSwitch(int key)
-    {
-        // Convert key (1-based) to index (0-based)
-        int index = key - 1;
+    // private void HandleCameraSwitch(int key)
+    // {
+    //     // Convert key (1-based) to index (0-based)
+    //     int index = key - 1;
 
-        if (index < 0 || index >= playerCameras.Length) return;
+    //     if (index < 0 || index >= playerCameras.Length) return;
 
-        // Reset all to low priority, set target to high
-        for (int i = 0; i < playerCameras.Length; i++)
-        {
-            playerCameras[i].Priority = (i == index) ? 20 : 10;
-        }
-    }
-    public override void OnNetworkDespawn()
-    {
-        if (IsOwner && InputHandler.Singleton != null)
-        {
-            InputHandler.Singleton.OnNumkeyTriggered -= HandleCameraSwitch;
-        }
-    }
+    //     // Reset all to low priority, set target to high
+    //     for (int i = 0; i < playerCameras.Length; i++)
+    //     {
+    //         playerCameras[i].Priority = (i == index) ? 20 : 10;
+    //     }
+    // }
+    // public override void OnNetworkDespawn()
+    // {
+    //     if (IsOwner && InputHandler.Singleton != null)
+    //     {
+    //         InputHandler.Singleton.OnNumkeyTriggered -= HandleCameraSwitch;
+    //     }
+    // }
 }
