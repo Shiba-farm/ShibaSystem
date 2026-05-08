af40cec0d4637d987cfbb98be67af2e6815a3d90
PongkhunP <139308094+PongkhunP@users.noreply.github.com>
2026-05-08 13:00:22 +0700

    Add[CurrencyManager] : add currency manager to handle the currency in game
    - centralise currency storage that every client can pull to display and utilize through RPC method call

---
+++ Assets/ScriptMain/Currency/CurrencyData.cs
@@ -0,0 +1,47 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class CurrencyData : NetworkBehaviour
+{
+    [SerializeField] private CurrencySignal currencySignal;
+    private NetworkVariable<long> gold = new NetworkVariable<long>(
+        500,
+        NetworkVariableReadPermission.Owner,  // only owner sees their balance
+        NetworkVariableWritePermission.Server  // only server can change it
+    );
+
+    public long Gold => gold.Value;
+    public override void OnNetworkSpawn()
+    {
+        gold.OnValueChanged += OnGoldChanged;
+
+        currencySignal.UpdateGold(gold.Value);
+    }
+    public override void OnNetworkDespawn()
+    {
+        gold.OnValueChanged -= OnGoldChanged;
+    }
+    private void OnGoldChanged(long previousValue, long newValue)
+    {
+        currencySignal.UpdateGold(newValue);
+    }
+    public void AddCurrency(long amount)
+    {
+        if (!IsServer) return;
+        gold.Value += amount;
+    }
+    public void ReduceCurrency(long amount)
+    {
+        if (!IsServer) return;
+        gold.Value -= amount;
+    }
+
+    public bool TrySpendCurrency(long amount)
+    {
+        if (!IsServer) return false;
+        if (gold.Value < amount) return false;
+
+        gold.Value -= amount;
+        return true;
+    }
+}
---
+++ Assets/ScriptMain/Currency/CurrencyManager.cs
@@ -0,0 +1,36 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class CurrencyManager : NetworkBehaviour
+{
+    public static CurrencyManager Instance { get; private set; }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private CurrencyData currencyStorage;
+    public long CurrentGold => currencyStorage.Gold;
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    public void AddCurrencyServerRpc(long newAmount, RpcParams rpcParams = default)
+    {
+        ulong clientId = rpcParams.Receive.SenderClientId;
+        currencyStorage.AddCurrency(newAmount);
+        Debug.Log($"Client {clientId} updated their gold to {newAmount}");
+
+    }
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    public void DeductCurrencyServerRpc(long newAmount, RpcParams rpcParams = default)
+    {
+        ulong clientId = rpcParams.Receive.SenderClientId;
+        currencyStorage.ReduceCurrency(newAmount);
+        Debug.Log($"Client {clientId} updated their gold to {newAmount}");
+
+    }
+}
